561f5070be58985749ec172931d5129b79fb4305
yabuJunn <juancho04reyes@gmail.com>
2025-06-09 20:43:57 -0500

    feat: basic structure dashboard

--- src/pages/dashboardPage/dashboardPage.tsx
+++ src/pages/dashboardPage/dashboardPage.tsx
@@ -6,6 +6,17 @@ export const DashboardPage = () => {
     return <>
         <main className="page" id="dashboardPage">
             <GlobalSideBar></GlobalSideBar>
+
+            <div className="dashboardPageContent">
+                <div className="dashboardPageHeader">
+                    <h1>¡Hola, Sofia!</h1>
+                    <p>Desde aquí podrás gestionar tus viajes, ver tus recomendaciones y mucho más.</p>
+                </div>
+
+                <div className="dashboardPageMainContent">
+
+                </div>
+            </div>
         </main>
     </>
 }
